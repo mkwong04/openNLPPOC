6ddf6c9b2751ebe9bdea256e1042143ee834296c
wongmk <MinKeat.Wong@centurylink.com>
2017-06-21 16:19:48 +0800

    added Part of Speech NLP

--- src/test/java/TestModel.java
+++ src/test/java/TestModel.java
@@ -1,5 +1,3 @@
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
 import java.io.InputStream;
 
 import opennlp.tools.doccat.DoccatModel;
@@ -9,14 +7,14 @@ public class TestModel{
 	
 	public static final void main(String[] args){
 		
-		try(InputStream is = new BufferedInputStream(new FileInputStream("trainedModel"))){
+		try(InputStream is = TestModel.class.getResourceAsStream("trainedModel")){
 								
 			DoccatModel m = new DoccatModel(is);
 			
 			DocumentCategorizerME myCategorizer = new DocumentCategorizerME(m);
 			System.out.println(args[0]);
 
-			double[] outcomes = myCategorizer.categorize(args);
+			double[] outcomes = myCategorizer.categorize(args[0].split(" "));
 			System.out.println(String.valueOf(outcomes));
 			
 			String allResults = myCategorizer.getAllResults(outcomes);
---
+++ src/test/java/TestPOSModel.java
@@ -0,0 +1,34 @@
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import opennlp.tools.doccat.DoccatModel;
+import opennlp.tools.doccat.DocumentCategorizerME;
+import opennlp.tools.postag.POSModel;
+import opennlp.tools.postag.POSTaggerME;
+
+public class TestPOSModel{
+	
+	public static final void main(String[] args){
+		
+		try(InputStream is = TestPOSModel.class.getResourceAsStream("en-pos-maxent.bin")){
+								
+			POSModel m = new POSModel(is);
+			
+			POSTaggerME myPOSTagger = new POSTaggerME(m);
+			System.out.println(args[0]);
+			
+			String[] sent = args[0].split(" ");
+
+			String tags[] = myPOSTagger.tag(sent);
+			double probs[] = myPOSTagger.probs();
+
+			for(int idx=0; idx<tags.length; idx++){
+				System.out.println(tags[idx]+" : "+probs[idx]);
+			}
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+}
